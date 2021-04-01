570f293b746b81825a93bf485fd6d1cc64d0509a
Pablo Portillo <oswalxtiyo@gmail.com>
2021-04-01 19:47:18 +0000

    original commit

--- index.js
+++ index.js
@@ -1,39 +1,61 @@
 const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
+const {Datastore} = require('@google-cloud/datastore');
 require('@google-cloud/debug-agent').start({ allowExpressions: true });
+const { v4: uuidv4 } = require('uuid');
 
 app.use(bodyParser.json());
 
 const port = process.env.PORT || 8080;
+
+const datastore = new Datastore();
+
 app.listen(port, () => {
   console.log('Listening on port', port);
 });
 
 app.get('/', async (req, res) => {
-    console.log(`Email Service: health check`);
-    res.status(200).send('Health check..');
+    console.log(`Out Of Stock Service: health check`);
+    res.status(200).send('OK');
 })
 
-app.post('/', async (req, res) => {
-  const notification = decodeBase64Json(req.body.message.data);
+app.post('/', async (req, res) => 
+{
+  const notification = req.body;
+  console.log(`Request ${notification}`)
   try 
   {
-    console.log(`Email Service: Report ${notification.id} trying...`);
-    sendEmail();
-    console.log(`Email Service: Report ${notification.id} success :-)`);
-    res.status(204).send();
+    console.log(`Out of stock report ${notification.product.id}`);
+    saveNotification(notification.product);
+    console.log(`Out of stock notification saved ${notification.product.id}`);
+    res.status(200).send();
   }
   catch (ex) {
-    console.log(`Email Service: Report ${notification.id} failure: ${ex}`);
+    console.log(`Out of stock notification error ${notification.product.id} failure: ${ex}`);
     res.status(500).send();
   }
 })
 
-function decodeBase64Json(data) {
-  return JSON.parse(Buffer.from(data, 'base64').toString());
-}
+function saveNotification(product) 
+{
+  console.log('Saving notification');
 
-function sendEmail() {
-  console.log('Sending email');
+  var id = uuidv4();
+
+  const kind = 'notification';
+
+  // The Cloud Datastore key for the new entity
+  const notificationKey = datastore.key([kind, id]);
+
+  const notification = {
+      key: notificationKey,
+      data: 
+      {
+        product: product
+      },
+    };
+    // [END datastore_entity_with_parent]
+
+    return datastore.save(notification);
 }
